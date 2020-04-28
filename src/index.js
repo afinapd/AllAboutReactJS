265e6a7995daafe3d0580877872320c4d4d7eadb
Afina Putri Dayanti <afnpd03@gmail.com>
2020-04-29 00:22:52 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+const PageOne = () => {
+  return (
+    <div>Page 1
+      <Link to="/pageTwo">Ke Halaman 2</Link>
+    </div>)
+};
+const PageTwo = () => {
+  return (
+    <div>Page 2
+      <Link to="/">Ke Halaman Awal</Link>
+    </div>)
+};
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={PageOne} />
+            <Route path="/pageTwo" component={PageTwo} />
+          </div>
+        </BrowserRouter>
+      </div>)
+  }
+}
+export default App;
